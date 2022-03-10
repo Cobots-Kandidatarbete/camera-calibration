59abacf9f80275d272216725658c12dfbaf74479
gasparyanartur <gasparyanartur99@gmail.com>
2022-03-10 14:45:37 +0100

    Fix calibration

--- cam_calibrator.py
+++ cam_calibrator.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np
 import cv2
-import glob
+
 
 cam_name = "realsense_d435"
 img_dir = f"{cam_name}_imgs"
+config_dir = f"{cam_name}_configs"
+config_name = f"{cam_name}_config"
 square_length = 2.4
 board_shape = 7, 10
 n_corners = n_corners_y, n_corners_x = board_shape[0] - 1, board_shape[1] - 1
@@ -44,8 +46,8 @@ object_points_3D = np.zeros((n_corners_y * n_corners_x, 3), np.float32)
 object_points_3D[:,:2] = np.mgrid[:n_corners_y, :n_corners_x].T.reshape(-1, 2)
 object_points_3D *= square_length
 
-def get_calibration_matrix(display=False):
-    cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+def get_calibration_matrix(display=False, undistort_test=False, save_file=False):
+    cv2.namedWindow("Image", cv2.WINDOW_AUTOSIZE)
 
     test_img = get_test_image()
     img_size = test_img.shape[:2]
@@ -65,17 +67,51 @@ def get_calibration_matrix(display=False):
 
             if display:
                 cv2.imshow("Image", image)
-                cv2.waitKey(200)
+                cv2.waitKey(1000)
 
-    _, camera_matrix, distortion, rotation_vectors, translation_vectors = cv2.calibrateCamera(world_frames, camera_frames, img_grayscale.shape[::-1], None, None)
-    optimal_camera_matrix, region = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion, img_size, alpha, img_size)
+    _, camera_matrix, distortion_matrix, rotation_vectors, translation_vectors = cv2.calibrateCamera(world_frames, camera_frames, img_grayscale.shape[::-1], None, None)
+    optimal_camera_matrix, region = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_matrix, img_size[::-1], alpha, img_size[::-1])
+    print(img_size[::-1])
 
-    return camera_matrix, distortion, optimal_camera_matrix
+    if undistort_test:
+        undistorted_test_img = cv2.undistort(test_img, camera_matrix, distortion_matrix, None, optimal_camera_matrix)
+
+        cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+
+        while True:
+            key_input = cv2.waitKey(0 & 0xFF)
+            if key_input == ord('1'):
+                cv2.imshow("Image", test_img)
+            elif key_input == ord('2'):
+                cv2.imshow("Image", undistorted_test_img)
+            elif key_input == ord('q'):
+                break
+
+
+    if save_file:
+        if config_dir not in os.listdir():
+            os.mkdir(config_dir)
+            print(f"Created {config_dir}")
+
+            config_path = f"{config_dir}/{config_name}.yaml"
+            config_file = cv2.FileStorage(config_path, cv2.FILE_STORAGE_WRITE)
+            config_file.write('camera_matrix', camera_matrix)
+            config_file.write('distortion_matrix', distortion_matrix)
+            config_file.write('optimal_camera_matrix', optimal_camera_matrix)
+            config_file.release()
+
+
+    cv2.destroyAllWindows()
+
+    return camera_matrix, distortion_matrix, optimal_camera_matrix
 
 
 def main():
-    camera_matrix, distortion, optimal_camera_matrix = get_calibration_matrix()
+    camera_matrix, distortion, optimal_camera_matrix = get_calibration_matrix(display=False, undistort_test=False, save_file=True)
+    print("Camera matrix:")
     print(camera_matrix)
+    print("Optimal camera matrix:")
+    print(optimal_camera_matrix)
 
 
 if __name__ == "__main__":
